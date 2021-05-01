d07653a221b2a3c22a821f71ab0c3555b83aae76
hitomi <sky.force.1013@gmail.com>
2021-05-01 16:41:36 +0900

    コメント追加

---
+++ keisan.rb
@@ -0,0 +1,13 @@
+a = gets.to_i
+b = gets.to_i
+# 入力したものを数値化
+
+puts a * b
+puts a + b
+
+a, b = gets.split.map(&:to_i)
+# gets.splitで入力したものを空白で区切って配列
+# map 配列の各要素を引数としてブロックを評価、結果を配列にして返す
+# &はこれを引数ですと伝える効果がある
+puts a * b
+puts a + b
